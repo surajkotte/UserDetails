d39a78a89a8730bd2428eed1302fe8c56fa1b0ed
surajkotte <surajkotte9640@gmail.com>
2024-05-28 22:42:58 +0530

    added accordian for posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,6 +4,7 @@ import { POSTS_URL } from "../constants/constant";
 import PostsDetails from "./PostsDetails";
 const Posts = (props) => {
   const [posts, SetPosts] = useState([]);
+  const [commentIndex, SetCommentIndex] = useState(0);
   const fetchPosts = async () => {
     const data = await fetch(POSTS_URL + props.userId);
     const jsonData = await data.json();
@@ -15,7 +16,7 @@ const Posts = (props) => {
   }, []);
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-[2]">
-      <div className=" h-4/5 w-2/4 bg-white p-4 rounded shadow-md overflow-y-auto">
+      <div className=" h-4/5 lg:w-2/4 md:w-3/4 sm:3/4 bg-white p-4 rounded shadow-md overflow-y-auto">
         <div className=" h-[50px]">
           <button
             className="text-blue-500 hover:text-blue-700 focus:outline-none float-right"
@@ -27,8 +28,16 @@ const Posts = (props) => {
           </button>
         </div>
         <div className=" flex justify-center items-center flex-col gap-2">
-          {posts.map((data) => {
-            return <PostsDetails postDetails={data} />;
+          {posts.map((data, index) => {
+            return (
+              <PostsDetails
+                postDetails={data}
+                commentsClicked={() => {
+                  SetCommentIndex(data.id);
+                }}
+                showComment={data.id == commentIndex ? true : false}
+              />
+            );
           })}
         </div>
       </div>
--- src/components/PostsDetails.js
+++ src/components/PostsDetails.js
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import CommentIcon from "@mui/icons-material/Comment";
+import IconButton from "@mui/material/IconButton";
 const PostsDetails = (props) => {
   return (
-    <div className=' w-4/5 h-32 border-2 justify-center items-center'>
-        <p>{props.postDetails.title}</p>
-        <p>{props.postDetails.body}</p>
+    <div className="h-33 border-2 rounded-2 p-4">
+      <p className="font-bold float-left">{props.postDetails.title}</p>
+      <p>{props.postDetails.body}</p>
+      <IconButton
+        aria-label="delete"
+        onClick={() => {
+          props.commentsClicked(props.postDetails.id);
+        }}
+      >
+        <CommentIcon />
+      </IconButton>
+      {props.showComment && <div>comments</div>}
     </div>
-  )
-}
+  );
+};
 
-export default PostsDetails
+export default PostsDetails;
